4d07afe576ae87c794f2ae2ce510ceb503bf82e5
harishashilu <67001684+harishashilu@users.noreply.github.com>
2020-06-16 16:29:25 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+/* This is a simple Java program. 
+FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+	// Your program begins with a call to main(). 
+	// Prints "Hello harish u r handsome, World" to the terminal window. 
+	public static void main(String args[]) 
+	{ 
+		System.out.println("Hello, World"); 
+	} 
+} 
